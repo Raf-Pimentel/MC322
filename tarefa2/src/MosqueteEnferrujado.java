4916891d63ebf629236135f07c73f7d7b192251e
Soon-To-Be <matheusboazao.mbs@gmail.com>
2025-09-18 21:50:32 -0300

    Comentarios

--- tarefa2/src/MosqueteEnferrujado.java
+++
@@ -1,9 +0,0 @@
-
-public class MosqueteEnferrujado extends Arma {
-
-    // Construtor do MosqueteEnferrujado
-    public MosqueteEnferrujado() {
-        // super(dano, minNivel)
-        super(14, 2);
-    }
-}
